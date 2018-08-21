a0caa3ec05db5fbba6a0d7e8045f08df4149feeb
qboileau <quentin.boileau@gmail.com>
2018-08-21 18:00:03 +0200

    wip continue testing stow

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,50 +1,54 @@
 use super::*;
 use std::fs::*;
+use std::panic;
 
-pub const TEST_SOURCE: &'static str = "/tmp/source";
-pub const TEST_TARGET: &'static str = "/tmp/target";
+pub const TESTS_DIRECTORY: &'static str = "/tmp/rstow-tests";
 
-pub fn build_source_target_directories() -> io::Result<()> {
-    println!("Create test directories");
+pub fn build_source_directory(name: &str) -> io::Result<PathBuf> {
+    println!("Create test source directory");
+    let source: PathBuf = PathBuf::from(TESTS_DIRECTORY.to_owned() + "/" + name).join("source");
+    create_dir_all(source.as_path()).unwrap();
+    Ok(source)
+}
 
-    let source: PathBuf = PathBuf::from(TEST_SOURCE);
-    let target: PathBuf = PathBuf::from(TEST_TARGET);
-    create_dir_all(source.as_path());
-    create_dir_all(target.as_path());
-    File::create(source.join("file.txt").as_path());
+pub fn build_target_directory(name: &str) -> io::Result<PathBuf> {
+    println!("Create test target directory");
+    let target: PathBuf = PathBuf::from(TESTS_DIRECTORY.to_owned() + "/" + name).join("target");
+    create_dir_all(target.as_path()).unwrap();
+    Ok(target)
+}
+
+pub fn clear_directory(path: &Path) -> io::Result<()> {
+    println!("Clean test directory {}", path.display());
+    if path.exists() { remove_dir_all(path).unwrap() }
     Ok(())
 }
 
-pub fn clear_directories() -> io::Result<()> {
-    println!("Clean test directories");
-    let source: PathBuf = PathBuf::from(TEST_SOURCE);
-    let target: PathBuf = PathBuf::from(TEST_TARGET);
-    remove_dir_all(target);
-    remove_dir_all(source);
-    Ok(())
-}
-
-pub fn add_file_to(name: &str, path: &PathBuf) -> io::Result<PathBuf> {
+pub fn add_file_to(name: &str, path: &Path) -> io::Result<PathBuf> {
     let file_path = path.join(name);
     println!("Add file {} in {} directory", file_path.display(), path.display());
     File::create(file_path.as_path());
     Ok(file_path)
 }
 
-pub fn add_directory_to(name: &str, path: &PathBuf) -> io::Result<PathBuf> {
+pub fn add_directory_to(name: &str, path: &Path) -> io::Result<PathBuf> {
     let dir_path = path.join(name);
     println!("Add directory {} in {} directory", dir_path.display(), path.display());
     create_dir_all(dir_path.as_path());
     Ok(dir_path)
 }
 
-pub fn with_test_directories(test: impl FnOnce(&PathBuf, &PathBuf) -> ()) {
-    build_source_target_directories();
+pub fn with_test_directories(name: &str, test: impl FnOnce(&PathBuf, &PathBuf) -> () + std::panic::UnwindSafe) -> io::Result<()>  {
 
-    let source: PathBuf = PathBuf::from(TEST_SOURCE);
-    let target: PathBuf = PathBuf::from(TEST_TARGET);
+    let test_dir = PathBuf::from(TESTS_DIRECTORY.to_owned() + "/" + name);
+    let source: PathBuf = build_source_directory(name).unwrap();
+    let target: PathBuf = build_target_directory(name).unwrap();
 
-    test(&source, &target);
+    let result = panic::catch_unwind(|| {
+        test(&source, &target);
+    });
 
-    clear_directories();
+    clear_directory(test_dir.as_path()).unwrap();
+    assert!(result.is_ok());
+    Ok(())
 }
